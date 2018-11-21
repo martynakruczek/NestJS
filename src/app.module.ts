71751782e8da25659a171e5756ccb9cc59db9b7c
Martyna <martyna.kruczek@iteo.com>
2018-11-21 12:45:02 +0100

    inital

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SharedModule } from './shared/shared.module';
+import { ConfigurationService } from './shared/configuration/configuration.service';
+import { type } from 'os';
+import { Configuration } from './shared/configuration/configuration.enum';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [SharedModule, MongooseModule.forRoot(ConfigurationService.connectionString)],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+
+  static host: string;
+  static port: number | string;
+  static isDev: boolean;
+
+  constructor(private readonly _configurationService: ConfigurationService){
+    AppModule.port = AppModule.normalizePort(_configurationService.get(Configuration.PORT));
+    AppModule.host = _configurationService.get(Configuration.HOST);
+    AppModule.isDev = _configurationService.isDevelopment;
+  }
+
+  private static normalizePort(param: number | string): number | string{
+    const portNumber: number = typeof param === 'string' ? parseInt(param, 10) : param;
+    if (isNaN(portNumber)) return param;
+    else if (portNumber >= 0) return portNumber;
+  }
+}
